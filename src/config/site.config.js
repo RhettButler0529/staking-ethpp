5c1d7546e93f5869000fe25cce229509f97d822a
ruslan <karabetrus@gmail.com>
2022-07-24 18:53:37 +0900

    update project

---
+++ src/config/site.config.js
@@ -0,0 +1,12 @@
+// const contactAddress = "0xd1f6b92510e3614d4d6c1b30b6084fd452cc74d3";
+const contactAddress = "0x9195782267390afab4691fc63a2956c30f8ec93f";
+
+const http = "https://api.avax.network/ext/bc/C/rpc";
+const chainId = 43114;
+const chainHex = '0xA86A';
+export {
+  contactAddress,
+  http,
+  chainId,
+  chainHex
+};
